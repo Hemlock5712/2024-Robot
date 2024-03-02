381870fa1b75488d70842609d40115e31061d8dd
TimR26 <26reid@hemlockps.com>
2024-03-02 12:19:29 -0500

    Add upper intake 2 sensor

--- src/main/java/frc/robot/subsystems/lineBreak/LineBreakIODigitalInput.java
+++ src/main/java/frc/robot/subsystems/lineBreak/LineBreakIODigitalInput.java
@@ -7,7 +7,7 @@ import frc.robot.subsystems.lineBreak.LineBreakHelper.LineBreakValues;
 public class LineBreakIODigitalInput implements LineBreakIO {
   DigitalInput lowerIntakeSensor = new DigitalInput(5);
   DigitalInput upperIntake1Sensor = new DigitalInput(3);
-  DigitalInput upperIntake2Sensor = new DigitalInput(4);
+  AnalogInput upperIntake2Sensor = new AnalogInput(3);
   AnalogInput magazine1Sensor = new AnalogInput(2);
   AnalogInput magazine2Sensor = new AnalogInput(1);
   AnalogInput magazine3Sensor = new AnalogInput(0);
@@ -17,7 +17,7 @@ public class LineBreakIODigitalInput implements LineBreakIO {
         new LineBreakValues(
             !lowerIntakeSensor.get(),
             !upperIntake1Sensor.get(),
-            !upperIntake2Sensor.get(),
+            upperIntake2Sensor.getVoltage() > 1,
             magazine1Sensor.getVoltage() > 1,
             magazine2Sensor.getVoltage() > 1,
             magazine3Sensor.getVoltage() > 1);
