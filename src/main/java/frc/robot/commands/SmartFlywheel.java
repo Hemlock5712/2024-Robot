5bacb56b9bc46bbcf66d78e25487e304dcb168a6
TimR26 <26reid@hemlockps.com>
2024-04-08 17:28:07 -0400

    Post comp

--- src/main/java/frc/robot/commands/SmartFlywheel.java
+++ src/main/java/frc/robot/commands/SmartFlywheel.java
@@ -36,7 +36,7 @@ public class SmartFlywheel extends Command {
       return;
     }
     if (SmartController.getInstance().getDriveModeType() == DriveModeType.CLIMBER) {
-      flywheel.setSpeedRotPerSec(4);
+      flywheel.setSpeedRotPerSec(3.6);
       return;
     }
     if (SmartController.getInstance().isSmartControlEnabled()) {
